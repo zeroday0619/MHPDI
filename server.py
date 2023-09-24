c1be77f82c582588ee11327ea4e32fec18808fa6
Euiseo Cha <zeroday0619_dev@outlook.com>
2023-09-24 15:55:04 +0900

    feat: change host

--- server.py
+++ server.py
@@ -20,4 +20,4 @@ async def read_root():
     return {"message": True}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
