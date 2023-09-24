0d31b756e1f5bd798ef8b7766255bcf0f4e90593
Euiseo Cha <zeroday0619_dev@outlook.com>
2023-09-24 15:53:44 +0900

    feat: add cors middleware

--- server.py
+++ server.py
@@ -2,9 +2,16 @@ import uvicorn
 from api import root
 from src.loader import DATALoader
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app_data = DATALoader()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://mental.menhera.kr"],
+    allow_credentials=True,
+)
 app.include_router(router=root, prefix="/api")
 
 
